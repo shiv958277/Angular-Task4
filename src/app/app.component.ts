9610eba668454420e6aa3dc246814ec3ba09fc9c
Your Name <vshiv1479@gmail.com>
2021-11-13 20:17:14 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App';
+
+  a: number = 0.259;
+  b: number = 1.3495;
+
+  birthday = new Date(1988, 3, 15); // April 15, 1988 -- since month parameter is zero-based
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainheaderComponent } from './application/mainheader/mainheader.component';
+import { LoginComponent } from './application/login/login.component';
+
+import {RouterModule,  Routes} from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent  },
+  { path: 'mainheader', component: MainheaderComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainheaderComponent,
+    LoginComponent,
+    RouterModule.forRoot(routes)
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  exports: [RouterModule],
+  providers: [],
+
 })
 export class AppModule { }
